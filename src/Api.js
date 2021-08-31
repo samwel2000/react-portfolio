bf8a60ee0f0654754f39520dad0b8e19523cc32b
samwel2000 <samwelgfrey@gmail.com>
2021-08-30 22:14:18 -0500

    -

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:8000/api/"
-const BASE_URL = "https://portfoliobackend.pythonanywhere.com/"
+const BASE_URL = "https://portfoliobackend.pythonanywhere.com/api/"
 export const HEROENDPOINT = 'hero-content'
 export const ABOUTENDPOINT = 'about-content'
 export const ORGANIZATIONENDPOINT = 'experience-organization'
@@ -13,7 +13,7 @@ export const BLOGCATEGORIESENDPOINT = 'posts-category'
 export const BLOGSENDPOINT = 'posts'
 
 const instance = axios.create({
-    baseURL:`${BASE_URL}`
+    baseURL: `${BASE_URL}`
 });
 
 export default instance
