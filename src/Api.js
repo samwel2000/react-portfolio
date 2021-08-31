97e72da2df3179ddd650d2c6cbf8ac67df7ac0ac
samwel2000 <samwelgfrey@gmail.com>
2021-08-30 21:48:18 -0500

    -

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/"
-// const BASE_URL = "https://portifoliobackend.pythonanywhere.com/"
+// const BASE_URL = "http://localhost:8000/api/"
+const BASE_URL = "https://portfoliobackend.pythonanywhere.com/"
 export const HEROENDPOINT = 'hero-content'
 export const ABOUTENDPOINT = 'about-content'
 export const ORGANIZATIONENDPOINT = 'experience-organization'
